bf4b8dadef7dcf614c41d136b2751e8cc9ff51aa
Lokesht99 <98078869+Lokesht99@users.noreply.github.com>
2022-02-08 09:02:19 +0530

    Add files via upload

---
+++ Index.py
@@ -0,0 +1,28 @@
+print("Lokesh Tunuguntla!!!")
+
+str='Lokesh Tunuguntla!!!'
+print(str)
+print(str[2:7])
+print(str[-4])
+print(str[-4:])
+print(str[-6:-1])
+print(str*4)
+print(str + "good boy" + str)
+
+a=10
+b=10.9
+c='lokesh'
+print(a)
+print(b)
+print(c)
+
+num=3
+if num>0:
+    print(num,"is a postive num.")
+print("i am outside the block.")
+
+num=-2
+if num>0:
+    printf("num,is a +ve num.")
+print("i am also outside the block.")
+
